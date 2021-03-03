9bbbc6d466f2a261aa43ca6d68e156416291ab90
laszloba <laszloba@users.noreply.github.com>
2021-03-03 22:34:13 +0100

    Refactor news list items

--- app/src/main/java/hu/laszloba/fortnightly/ui/list/NewsListSmallItemView.kt
+++ app/src/main/java/hu/laszloba/fortnightly/ui/list/NewsListSmallItemView.kt
@@ -3,21 +3,12 @@ package hu.laszloba.fortnightly.ui.list
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.LinearLayout
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import hu.laszloba.fortnightly.R
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import hu.laszloba.fortnightly.databinding.ListItemNewsSmallBinding
-import hu.laszloba.fortnightly.model.NewsListItemPresentationModel
 
-// TODO similar to NewsListLargeItemView
-class NewsListSmallItemView : ConstraintLayout, BaseNewsListItemView {
-
-    override var onItemClickedListener: NewsListAdapter.OnItemClickedListener? = null
-
-    private val binding =
-        ListItemNewsSmallBinding.inflate(LayoutInflater.from(context), this)
+class NewsListSmallItemView : BaseNewsListItem {
 
     constructor(context: Context) : super(context)
 
@@ -26,30 +17,14 @@ class NewsListSmallItemView : ConstraintLayout, BaseNewsListItemView {
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) :
         super(context, attrs, defStyleAttr)
 
-    init {
-        layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
+    private val binding =
+        ListItemNewsSmallBinding.inflate(LayoutInflater.from(context), this)
 
-        val padding = resources.getDimension(R.dimen.margin_padding_size_medium).toInt()
-        setPadding(paddingLeft, padding, paddingRight, padding)
-    }
+    override fun getViewForClickListener(): View = binding.root
 
-    override fun bind(model: NewsListItemPresentationModel) {
-        with(binding) {
-            Glide.with(context)
-                .load(model.urlToImage)
-                .transition(DrawableTransitionOptions.withCrossFade())
-                .into(imageImageView)
+    override fun getImageImageView(): ImageView = binding.imageImageView
 
-            hoursAgoTextView.text =
-                context.getString(R.string.news_list_item_hours_ago, model.hoursAgo)
-            titleTextView.text = model.title
+    override fun getHoursAgoTextView(): TextView = binding.hoursAgoTextView
 
-            root.setOnClickListener {
-                onItemClickedListener?.onItemClicked(model)
-            }
-        }
-    }
+    override fun getTitleTextView(): TextView = binding.titleTextView
 }
