b335d434d56eeb922eb284bfd647819927902e4e
laszloba <laszloba@users.noreply.github.com>
2021-03-06 12:24:10 +0100

    Create collapsible toolbar for news list screen

--- app/src/main/java/hu/laszloba/fortnightly/ui/list/NewsListFragment.kt
+++ app/src/main/java/hu/laszloba/fortnightly/ui/list/NewsListFragment.kt
@@ -8,16 +8,18 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import hu.laszloba.fortnightly.R
 import hu.laszloba.fortnightly.databinding.FragmentNewsListBinding
 import hu.laszloba.fortnightly.extension.exhaustive
 import hu.laszloba.fortnightly.model.NewsListItemPresentationModel
 import hu.laszloba.fortnightly.navigator.AppNavigator
+import hu.laszloba.fortnightly.ui.toolbar.ToolbarAnimator
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class NewsListFragment : Fragment() {
+class NewsListFragment : Fragment(), ToolbarAnimator.AnimationChangeListener {
 
     private object Flipper {
         const val LOADING = 0
@@ -29,6 +31,7 @@ class NewsListFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: NewsListViewModel by viewModels()
+    private val toolbarAnimator = ToolbarAnimator(this)
 
     private lateinit var listAdapter: NewsListAdapter
 
@@ -61,11 +64,26 @@ class NewsListFragment : Fragment() {
             layoutManager = listLayoutManager
             addItemDecoration(DividerItemDecoration(context, listLayoutManager.orientation))
             adapter = listAdapter
+            clearOnScrollListeners()
+            addOnScrollListener(
+                object : RecyclerView.OnScrollListener() {
+                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                        toolbarAnimator.calculateAnimation(dy)
+                    }
+                }
+            )
         }
 
         with(viewModel) {
             viewState.observe(viewLifecycleOwner, ::render)
             loadNewsList()
+        }
+    }
+
+    override fun onAnimationProgressChange(progress: Float) {
+        with(binding) {
+            mainMotionLayout.progress = progress
+            toolbarBackgroundImage.setShapeInterpolation(progress)
         }
     }
 
